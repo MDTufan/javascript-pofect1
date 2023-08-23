536d8ce779b273ccb9618b46d7ef120c9598ccf1
MDTufan <129486543+MDTufan@users.noreply.github.com>
2023-08-23 19:49:26 +0600

    Add files via upload
    password show

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+
+function myFunction(){
+    const value =document.getElementById("value");
+    if(value.type==="password"){
+        value.type="text"
+    }else{
+        value.type="password"
+    }
+};
